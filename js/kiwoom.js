8d4c2a49abade04a2ce90e42e93d907b01a43746
asdas <95152341+1213212132@users.noreply.github.com>
2023-08-21 15:23:28 +0900

    Add files via upload

--- js/kiwoom.js
+++ js/kiwoom.js
@@ -74,6 +74,35 @@ $(function () {
 
 
 
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    });
+
+    $('.gnb li>a').on('click', function (e) {
+        if ($('.gnb').hasClass('on')) {
+
+            //서버메뉴가 없으면 바로 클릭되게 하기
+            if ($(this).next().size() != 0) {
+                e.preventDefault();
+            }
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.sub').stop().slideUp();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on')
+    });
+
+    $('.gnb').on('wheel', function (e) {
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+        }
+    })
+
+
+
 })
 
 
